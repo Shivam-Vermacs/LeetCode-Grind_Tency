e86d7e438f617013b16b161979fdcccd4d052746
Shivam Verma <161146158+Shivam-Vermacs@users.noreply.github.com>
2025-06-25 18:08:12 +0530

    Time: 2744 ms (5.64%) | Memory: 34.5 MB (80.79%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        long long n=nums.size();
+        //brute force
+
+    //     long long longest=0,s=0;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=i;j<n;j++)
+    //         {
+    //             s=0;
+    //             for(int l=i;l<=j;l++)
+    //             {
+    //                 s+=nums[l];
+    //             }
+    //             if(s==k)
+    //             longest++;
+    //         }
+    //     }
+    //     return longest;
+        
+    // }
+
+
+    //Optimized Brute Force
+         long long longest=0,s=0;
+        for(int i=0;i<n;i++)
+        {s=0;
+            for(int j=i;j<n;j++)
+            {
+                
+               s+=nums[j];
+                if(s==k)
+                longest++;
+            }
+        }
+        return longest;
+        
+    }
+
+
+};
