5328dfa28f83e8496d8868a8d93059beb90e5776
Shivam Verma <161146158+Shivam-Vermacs@users.noreply.github.com>
2025-06-29 11:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.01%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,34 +2,55 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        int count1=0,count2=0,count3=0;
-        for(int i=0;i<n;i++)
+        // int count1=0,count2=0,count3=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //     {
+        //         count1++;
+        //     }
+        //     else
+        //     if(nums[i]==1)
+        //     {
+        //         count2++;
+        //     }
+        //     else
+        //     {
+        //         count3++;
+        //     }
+        // }
+        // for(int i=0;i<count1;i++)
+        // {
+        //     nums[i]=0;
+        // }
+        // for(int i=count1;i<count2+count1;i++)
+        // {
+        //     nums[i]=1;
+        // }
+        // for(int i=count2+count1;i<n;i++)
+        // {
+        //     nums[i]=2;
+        // }
+
+        int mid=0,low=0,high=n-1;
+        while(mid<=high)
         {
-            if(nums[i]==0)
+            if(nums[mid]==0)
             {
-                count1++;
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
             else
-            if(nums[i]==1)
+            if(nums[mid]==1)
             {
-                count2++;
+                mid++;
             }
             else
             {
-                count3++;
+                swap(nums[mid],nums[high]);
+                high--;
             }
-        }
-        for(int i=0;i<count1;i++)
-        {
-            nums[i]=0;
-        }
-        for(int i=count1;i<count2+count1;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i=count2+count1;i<n;i++)
-        {
-            nums[i]=2;
         }
     }
 };
